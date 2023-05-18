9256f8ae9bc616d5f5cc097fde5aa599a7e32df8
AsmitaTagad <117521897+AsmitaTagad@users.noreply.github.com>
2023-05-18 14:56:45 +0530

    created counterApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+import {
+  AiFillCaretUp,
+  AiFillCaretDown,
+  AiFillCaretLeft,
+  AiFillCaretRight,
+} from 'react-icons/ai';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-     div
+    <div className="container">
+      <div>
+        <AiFillCaretLeft size="2rem" onClick={() => setCount(count + 10)} />
+      </div>
+      <div>
+        <div>
+          <AiFillCaretUp size="2rem" onClick={() => setCount(count + 1)} />
+        </div>
+        <div style={{ marginLeft: '0.7rem',fontWeight:"600",fontSize:"2rem" }}>   <p>{count}</p>
+        </div>
+        <div>
+          <AiFillCaretDown size="2rem" onClick={() => setCount(count - 1)} />
+        </div>
+      </div>
+      <div>
+        <AiFillCaretRight size="2rem" onClick={() => setCount(count - 10)} />
+      </div>
     </div>
   );
 }
